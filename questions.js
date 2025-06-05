399b6d1505aacd51e2d5bae1e70f66abfa171653
michaelPedr <166827913+michaelPedr@users.noreply.github.com>
2025-06-05 18:06:09 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    image: "https://via.placeholder.com/400x250.png?text=Image+1",
+    reponses: [
+      { texte: "Comment parler aux pigeons", correct: false },
+      { texte: "Comment survivre à une attaque d'ours", correct: true },
+      { texte: "Comment danser avec un chapeau", correct: false },
+      { texte: "Comment devenir un arbre", correct: false }
+    ]
+  },
+  {
+    image: "https://via.placeholder.com/400x250.png?text=Image+2",
+    reponses: [
+      { texte: "Comment préparer un thé qui parle", correct: false },
+      { texte: "Comment s'excuser auprès d'un robot", correct: false },
+      { texte: "Comment cuisiner un repas pour 50 personnes", correct: true },
+      { texte: "Comment élever un moustique domestique", correct: false }
+    ]
+  }
+];
